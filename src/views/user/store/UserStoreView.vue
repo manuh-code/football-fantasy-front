ee279d28bfe5f128b629da1826a433c69a1d24e2
Ignacio Manuel Sanchez Neri <165204748+manu0989@users.noreply.github.com>
2026-06-19 14:30:38 -0600

    home menu en login y store user

--- src/views/user/store/UserStoreView.vue
+++ src/views/user/store/UserStoreView.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-import UserStoreComponent from '@/components/user/store/UserStoreComponent.vue'
+import UserStoreComponent from "@/components/user/store/UserStoreComponent.vue";
+import HomeMenu from "@/components/home/HomeMenu.vue";
 </script>
 
 <template>
   <div class="register-view">
     <UserStoreComponent />
   </div>
+  <HomeMenu />
 </template>
 
 <style lang="scss" scoped>
